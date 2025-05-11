1e1cc925feb3caf12d9077ae4250fcb88c89ecfb
joeyexists <joeybazar@icloud.com>
2025-05-10 20:06:54 -0500

    version change

--- CustomCardLoadout/Core.cs
+++ CustomCardLoadout/Core.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 using System.Reflection;
 using UnityEngine.SceneManagement;
 
-[assembly: MelonInfo(typeof(CustomCardLoadout.Core), "CustomCardLoadout", "1.0.2-beta", "joeyexists", null)]
+[assembly: MelonInfo(typeof(CustomCardLoadout.Core), "CustomCardLoadout", "1.0.3-beta", "joeyexists", null)]
 [assembly: MelonGame("Little Flag Software, LLC", "Neon White")]
 
 namespace CustomCardLoadout
